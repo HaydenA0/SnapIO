5e66d5b4e6d23f34abb6fe63d2483eb035deae51
HaydenA0 <aregrag2005@gmail.com>
2025-10-03 14:31:41 +0100

    filters

--- src/TODO.md
+++ src/TODO.md
@@ -1,16 +1,8 @@
 - Classes
     - [x] I/O (Input/Output): Loading from and saving to the disk (load_image, save_image).
-
     - [-] Inspection: Getting metadata about an image (inspect_image).
-
     - [x] Visualization: Displaying images to the user (show_image, compare_images).
-
-    - [ ] Processing/Analysis: Performing calculations on image data (calculate_normal_difference_of_images).
-
-- Inspection 
-    - [x] Basic inspector that shows the image size, format, dimensions, aspect ratio and encoding
-    - [x] Basic Statistics that shows the mean, std-dev, min and max pixel (per channel)
-    - [-] Basic Analysis that shows the average brightness, average contrast,histogram, laplacian_variance for contrast , [complexity: edge_percentage] to be searched,
+    - [x] Processing/Analysis: Performing calculations on image data (calculate_normal_difference_of_images).
 
 
 - Testing
--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,15 @@
 import numpy as np
 
 
+def rotate_kernel(kernel):
+    return np.rot90(kernel, 2)
+
+
+def convolution_kernel_segment(kernel, patch):
+    kernel_rotated = rotate_kernel(kernel)
+    return np.sum(kernel_rotated * patch)
+
+
 class Processor:
     def __init__(self) -> None:
         pass
@@ -15,3 +24,36 @@ class Processor:
                 f"The Image 1 has a size of {(x1,y1)} and the Image 2 has a size of {(x2,y2)}. \n Make sure they are the smae dimension. "
             )
         return np.sum((img1_bigger - img2_bigger) ** 2) / (x1 * y1)
+
+    def apply_kernel(self, image, kernel):
+        n, m = kernel.shape
+        output = np.zeros_like(image)
+        floor_n = n // 2
+        floor_m = m // 2
+        ceil_n = n - floor_n
+        ceil_m = m - floor_m
+        for i in range(floor_n, image.shape[0] - ceil_n + 1):
+            for j in range(floor_m, image.shape[1] - ceil_m + 1):
+                patch = image[i - floor_n : i + ceil_n, j - floor_m : j + ceil_m]
+                output[i, j] = convolution_kernel_segment(kernel, patch)
+
+            return output
+
+    def median_filter(self, image, grid_size: int = 3):
+        if not isinstance(image, np.ndarray):
+            raise TypeError("Input image must be a numpy array.")
+
+        if image.ndim != 2:
+            raise ValueError("Image must be 2D .")
+
+        result = image.copy()
+        floor_n = grid_size // 2
+        ceil_n = grid_size - floor_n
+        for i in range(floor_n, image.shape[0] - ceil_n + 1):
+            for j in range(floor_n, image.shape[1] - ceil_n + 1):
+                patch = image[i - floor_n : i + ceil_n, j - floor_n : j + ceil_n]
+                patch.sort()
+                median = patch[len(patch) // 2]
+                result[i, j] = median
+
+        return result
