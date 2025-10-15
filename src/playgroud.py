7015725c9441c0bf7315e0b4e5398c276ded88e8
HaydenA0 <aregrag2005@gmail.com>
2025-10-15 16:49:58 +0100

    Cant remember what i did here ngl

--- src/playgroud.py
+++ src/playgroud.py
@@ -3,13 +3,16 @@ from utilsio import ImageIO
 import numpy as np
 from inspection import ImageInspector
 from processing import Processor
+from visulization import Visualizer
 
 
 io = ImageIO()
 ii = ImageInspector()
 proc = Processor()
+vis = Visualizer()
 
 img = io.load_image("./images/grass.png", format="Grayscale")
+
 img = np.array(img, dtype=np.float64)
 kernel = np.ones((3, 3)) / 9
 
@@ -17,3 +20,5 @@ kernel = np.ones((3, 3)) / 9
 # Compare these 2
 oldconv = proc.apply_kernel_old(img, kernel)
 newconv = proc.apply_kernel_new(img, kernel)
+
+vis.compare_images([img, newconv], ["Image", "Blurred"])
